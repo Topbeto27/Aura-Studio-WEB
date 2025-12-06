dbc2484d24746675c80294176d79e2de5df15e9d
Topbeto27 <albertlinux27@gmail.com>
2025-12-05 19:27:10 -0600

    optimizacion de memoria

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    deviceSizes: [640, 750, 828, 1080],
+    imageSizes: [16, 32, 48, 64],
     remotePatterns: [
       {
         protocol: 'https',
